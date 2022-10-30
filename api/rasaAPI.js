4ce3bc69eaf50ad241154099f94706666a8f5cf4
rogerwaldvogel <climberwoodi@gmx.ch>
2022-10-30 10:11:34 +0100

    jslint error addressed.

--- api/rasaAPI.js
+++ api/rasaAPI.js
@@ -1,12 +1,12 @@
+import axios from 'axios';
 const express = require('express');
-import axios from "axios"
 
 const app = express();
 app.use(express.json());
 
 app.post('/messages', async (req, res) => {
-  let response = await axios.post(process.env.RASA_API + "/webhooks/rest/webhook", req.body)
+  const response = await axios.post(process.env.RASA_API + '/webhooks/rest/webhook', req.body);
   res.json(response.data);
 });
 
-module.exports = app
+module.exports = app;
