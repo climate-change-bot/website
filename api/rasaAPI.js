0ea50889f0215342130fa16b9264e454f7659987
rogerwaldvogel <climberwoodi@gmx.ch>
2022-10-30 11:03:21 +0100

    Removed all semicolon.

--- api/rasaAPI.js
+++ api/rasaAPI.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
-const express = require('express');
+import axios from 'axios'
+const express = require('express')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 app.post('/messages', async (req, res) => {
-  const response = await axios.post(process.env.RASA_API + '/webhooks/rest/webhook', req.body);
-  res.json(response.data);
-});
+  const response = await axios.post(process.env.RASA_API + '/webhooks/rest/webhook', req.body)
+  res.json(response.data)
+})
 
-module.exports = app;
+module.exports = app
