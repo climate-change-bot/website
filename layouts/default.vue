fd8d094b5f912b883f96a9ceb323ce536ac4ea68
rogerwaldvogel <climberwoodi@gmx.ch>
2022-11-09 15:20:30 +0100

    Added a default layout.

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <Nuxt/>
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+}
+#__nuxt {
+  height: 100%;
+}
+#__layout {
+  height: 100%;
+}
+</style>
