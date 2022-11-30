d9b98f5fc8a553792fa080fcb2b76a6ea82a1861
rogerwaldvogel <climberwoodi@gmx.ch>
2022-11-30 13:55:00 +0100

    Added contact information.

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <default-header/>
-    Coming soon :-)
+    <section class="bg-gray-50">
+      <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-40">
+        <div class="mr-auto place-self-center lg:col-span-7">
+          <div class="flex justify-center mb-8 mt-6 lg:hidden">
+            <a href="https://www.linkedin.com/in/roger-waldvogel/"
+               target="_blank">
+              <img src="/roger.jpg" alt="Roger Waldvogel" class="h-60 rounded-full">
+            </a>
+          </div>
+          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            Der Chatbot wird von mir <a href="https://www.linkedin.com/in/roger-waldvogel/" target="_blank">(Roger
+            Waldvogel)</a> im Rahmen meiner Master Thesis für den Studiengang <a
+            href="https://www.hslu.ch/en/lucerne-school-of-business/degree-programmes/master/applied-information-and-data-science/"
+            target="_blank"
+            class="font-semibold">Information
+            and Data Science</a> an der Hochschule Luzern entwickelt. Falls du Fragen, Anmerkungen oder interesse an
+            einer Zusammenarbeit hast, freue ich mich auf deine Kontaktaufnahme.
+          </p>
+          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            E-Mail: <br>
+            <a href="mailto:roger.waldvogel@stud.hslu.ch">roger.waldvogel@stud.hslu.ch</a>
+          </p>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex max-w-xs">
+          <a href="https://www.linkedin.com/in/roger-waldvogel/"
+             target="_blank">
+            <img src="/roger.jpg" alt="Roger Waldvogel" class="h-60 ml-32 rounded-full">
+          </a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
