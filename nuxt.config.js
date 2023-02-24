de55bc80463655f74e2df29488bb5e7c88dd1630
roger <climberwoodi@gmx.ch>
2023-02-24 13:09:32 +0100

    removed tangling coma

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/tooltip.js', mode: 'client' },
+    { src: '~/plugins/tooltip.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
